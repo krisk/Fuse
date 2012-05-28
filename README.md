65fd9307dcdf3967dc254e16ce9b4bee96275b70
krisk <kirollos@gmail.com>
2012-05-27 21:44:34 -0700

    Added instructions

--- README.md
+++ README.md
@@ -2,3 +2,57 @@ Fuse
 ====
 
 Fuzzy search over models
+
+#### Usage
+
+Suppose you have the following data:
+
+```javascript
+data = [{
+  id: 1,
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald'
+},{
+  id: 2,
+  title: 'The DaVinci Code',
+  author: 'Dan Brown'
+},{
+  id: 3,
+  title: 'Angels & Demons',
+  author: 'Dan Brown'
+}];
+```
+
+##### Example 1
+
+```javascript
+var options = {
+  values: ['author', 'title'],
+  id: 'id'
+}
+var f = new Fuse(data, options);
+var result = f.search('brwn');
+
+==> [2, 3];
+```
+
+##### Example 2
+
+```javascript
+var options = {
+  values: ['author', 'title']
+}
+var f = new Fuse(data, options);
+var result = f.search('brwn');
+
+==>
+[{
+  id: 1,
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald'
+}, {
+  id: 3,
+  title: 'Angels & Demons',
+  author: 'Dan Brown'
+}];
+```
--- fuse.js
+++ fuse.js
@@ -1,3 +1,21 @@
+/**
+ * Fuse - Fuzzy search on models
+ *
+ * Copyright (c) 2012 Kirollos Risk <kirollos@gmail.com>.
+ * All Rights Reserved. Apache Software License 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 (function() {
   /**
   * Adapted from "Diff, Match and Patch", by Google
@@ -14,9 +32,6 @@
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *   http://www.apache.org/licenses/LICENSE-2.0
   */
   function Searcher(pattern, options) {
       options = options || {};
