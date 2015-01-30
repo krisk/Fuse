33ec346943669bbfa249344c09ca307c6d15788e
Eduardo Matos <eduardo.matos@sieve.com.br>
2015-01-30 15:43:21 -0200

    remove console.* from minified file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-node_modules/uglify-js/bin/uglifyjs src/fuse.js -c -m --comments -o src/fuse.min.js
+node_modules/uglify-js/bin/uglifyjs src/fuse.js -c drop_console=true -m --comments -o src/fuse.min.js
 zip latest.zip src/*
