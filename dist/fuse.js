0c796b9fe69e1e5d5b2db95bc17234b37d3e9efb
zecaptus <steeve.pitis@gmail.com>
2017-06-07 11:35:21 +0200

    Add a match index for children

--- dist/fuse.js
+++ dist/fuse.js
@@ -409,10 +409,10 @@ module.exports = function (text, pattern, patternAlphabet, _ref) {
       expectedLocation: expectedLocation,
       distance: distance
     });
-    currentThreshold = Math.min(score, currentThreshold);
+    currentThreshold = Math.min(score, currentThreshold
 
     // What about in the other direction? (speed up)
-    bestLocation = text.lastIndexOf(pattern, expectedLocation + patternLen);
+    );bestLocation = text.lastIndexOf(pattern, expectedLocation + patternLen);
 
     if (bestLocation != -1) {
       var _score = bitapScore(pattern, {
@@ -491,11 +491,11 @@ module.exports = function (text, pattern, patternAlphabet, _ref) {
           currentLocation: currentLocation,
           expectedLocation: expectedLocation,
           distance: distance
-        });
+        }
 
         // This match will almost certainly be better than any existing match.
         // But check anyway.
-        if (finalScore <= currentThreshold) {
+        );if (finalScore <= currentThreshold) {
           // Indeed it is
           currentThreshold = finalScore;
           bestLocation = currentLocation;
@@ -735,6 +735,8 @@ var Fuse = function () {
     key: '_analyze',
     value: function _analyze(_ref2, _ref3) {
       var key = _ref2.key,
+          _ref2$keyIndex = _ref2.keyIndex,
+          keyIndex = _ref2$keyIndex === undefined ? -1 : _ref2$keyIndex,
           value = _ref2.value,
           record = _ref2.record,
           index = _ref2.index;
@@ -769,10 +771,10 @@ var Fuse = function () {
           for (var i = 0; i < tokenSearchers.length; i += 1) {
             var tokenSearcher = tokenSearchers[i];
 
-            this._log('\nPattern: "' + tokenSearcher.pattern + '"');
+            this._log('\nPattern: "' + tokenSearcher.pattern + '"'
 
             // let tokenScores = []
-            var hasMatchInText = false;
+            );var hasMatchInText = false;
 
             for (var j = 0; j < words.length; j += 1) {
               var word = words[j];
@@ -789,8 +791,9 @@ var Fuse = function () {
                   scores.push(1);
                 }
               }
-              this._log('Token: "' + word + '", score: ' + obj[word]);
+              this._log('Token: "' + word + '", score: ' + obj[word]
               // tokenScores.push(obj)
+              );
             }
 
             if (hasMatchInText) {
@@ -817,10 +820,10 @@ var Fuse = function () {
 
         var checkTextMatches = this.options.tokenize && this.options.matchAllTokens ? numTextMatches >= tokenSearchers.length : true;
 
-        this._log('\nCheck Matches: ' + checkTextMatches);
+        this._log('\nCheck Matches: ' + checkTextMatches
 
         // If a match is found, add the item to <rawResults>, including its score
-        if ((exists || mainSearchResult.isMatch) && checkTextMatches) {
+        );if ((exists || mainSearchResult.isMatch) && checkTextMatches) {
           // Check if the item already exists in our results
           var existingResult = resultMap[index];
 
@@ -829,6 +832,7 @@ var Fuse = function () {
             // existingResult.score, bitapResult.score
             existingResult.output.push({
               key: key,
+              keyIndex: keyIndex,
               score: finalScore,
               matchedIndices: mainSearchResult.matchedIndices
             });
@@ -838,6 +842,7 @@ var Fuse = function () {
               item: record,
               output: [{
                 key: key,
+                keyIndex: keyIndex,
                 score: finalScore,
                 matchedIndices: mainSearchResult.matchedIndices
               }]
@@ -850,6 +855,7 @@ var Fuse = function () {
         for (var _i3 = 0, len = value.length; _i3 < len; _i3 += 1) {
           this._analyze({
             key: key,
+            keyIndex: value.length > 1 ? _i3 : -1,
             value: value[_i3],
             record: record,
             index: index
@@ -919,6 +925,9 @@ var Fuse = function () {
             };
             if (item.key) {
               obj.key = item.key;
+            }
+            if (item.hasOwnProperty('keyIndex') && item.keyIndex > -1) {
+              obj.index = item.keyIndex;
             }
             data.matches.push(obj);
           }
