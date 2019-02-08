bf1c9fb599a8e8681159fd69ff31455653964e75
Salvatore Ravidà <salvoravida@yahoo.it>
2019-02-08 05:52:02 +0100

    [eval unsafe] fix build production only!! [urgent] (#283)
    * fix build production only!!
    
    eval( ) problem on production!! csp unsafe
    
    * revert build script
    
    * fix main export .min build production
    
    * fix dist production only mode

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,10 @@ const HOMEPAGE = pckg.homepage
 let copyright = fs.readFileSync('COPYRIGHT.txt', 'UTF8')
 
 module.exports = (env, argv) => {
-  const isProd = argv.mode === 'production'
+  const isMin = !!argv.minify
 
   let plugins = []
-  if (isProd) {
+  if (isMin) {
     plugins.push(new MinifyPlugin())
   }
   plugins = [
@@ -33,7 +33,7 @@ module.exports = (env, argv) => {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: `${LIBRARY_NAME}${isProd ? '.min' : ''}.js`,
+      filename: `${LIBRARY_NAME}${isMin ? '.min' : ''}.js`,
       library: 'Fuse',
       libraryTarget: 'umd',
       umdNamedDefine: true,
