6a80a2f2d7ef56bb2cc279341067edd1e02b1469
Jeroen Claassens <jeroen.claassens@live.nl>
2019-05-24 20:08:47 +0200

    Fix typings
    This fixes the typings for nested keys which was previously broken, and
    reported by @mmajko on #261
    
    While this does fix the typings at least for now, they can be made even
    more ideal. To achieve this however advanced typings would be required
    and a request has been made to the utility-types repository (see:
    https://github.com/piotrwitek/utility-types/issues/85) for the required
    advanced type. To summarize that issue, if we would have a Flatten<T>
    that would flatten the entire given typing T to single keys it would be
    a perfect match for this library.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -30,7 +30,7 @@ declare namespace Fuse {
     shouldSort?: boolean;
     sortFn?: (a: { score: number }, b: { score: number }) => number;
     getFn?: (obj: any, path: string) => any;
-    keys?: (keyof T)[] | { name: keyof T; weight: number }[];
+    keys?: (keyof T)[] | T[keyof T] | { name: keyof T; weight: number }[];
     verbose?: boolean;
     tokenize?: boolean;
     tokenSeparator?: RegExp;
