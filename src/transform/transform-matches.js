31fc5b7c963149d568599270b0783cd03db7e7b1
Daniel Dickinson <cshored@thecshore.com>
2020-03-16 22:51:57 -0400

    Fix 'TypeError: Cannot read property 'length' of undefined (#360)
    Which occurs when pattern length exceeds machine word length and we get
    results but not matches (highlights).
    
    Signed-off-by: Daniel F. Dickinson <cshored@thecshore.com>

--- src/transform/transform-matches.js
+++ src/transform/transform-matches.js
@@ -1,11 +1,17 @@
+const { isArray, isDefined, isString, isNumber, isObject } = require('../helpers/type-checkers')
+
 module.exports = (result, data) => {
   const matches = result.matches
   data.matches = []
 
+  if (!isDefined(matches)) {
+    return
+  }
+
   for (let i = 0, len = matches.length; i < len; i += 1) {
     let match = matches[i]
 
-    if (match.indices.length === 0) {
+    if (!isDefined(match.indices) || match.indices.length === 0) {
       continue
     }
 
