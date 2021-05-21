bb27487447100428400559279c6959997c151ab1
Ayush Joshi <ayush854032@gmail.com>
2021-05-21 17:07:06 +0530

    chore(exception): add attribute 'message' to the custom exception class object
    add attribute 'message' to custom exception class object to save the error message in
    it and then use it later when required

--- src/core/error.js
+++ src/core/error.js
@@ -2,6 +2,7 @@ export class IncorrectIndexTypeException extends Error {
   constructor(message = "Incorrect 'index' type") {
     super(message);
     this.name = "IncorrectIndexTypeException";
+    this.message = message;
   }
 }
 
@@ -9,6 +10,7 @@ export class LogicalSearchUnavailableException extends Error {
   constructor(message = "Logical search is not available") {
     super(message);
     this.name = "LogicalSearchUnavailableException";
+    this.message = message;
   }
 }
 
@@ -16,6 +18,7 @@ export class ExtendedSearchUnavailableException extends Error {
   constructor(message = "Extended search is not available") {
     super(message);
     this.name = "ExtendedSearchUnavailableException";
+    this.message = message;
   }
 }
 
@@ -23,6 +26,7 @@ export class MissingKeyPropertyException extends Error {
   constructor(name) {
     super(`Missing ${name} property in key`);
     this.name = "MissingKeyPropertyException";
+    this.message = `Missing ${name} property in key`;
   }
 }
 
@@ -30,6 +34,7 @@ export class PatternLengthTooLargeException extends Error {
   constructor(max) {
     super(`Pattern length exceeds max of ${max}`);
     this.name = "PatternLengthTooLargeException";
+    this.message = `Pattern length exceeds max of ${max}`;
   }
 }
 
@@ -37,6 +42,7 @@ export class InvalidKeyWeightValueException extends Error {
   constructor(key) {
     super(`Property 'weight' in key '${key}' must be a positive integer`);
     this.name = "InvalidKeyWeightValueException";
+    this.message = `Property 'weight' in key '${key}' must be a positive integer`;
   }
 }
 
@@ -44,5 +50,6 @@ export class LogicalSearchInvalidQueryForKeyException extends Error {
   constructor(key) {
     super(`Invalid value for key ${key}`);
     this.name = "LogicalSearchInvalidQueryForKeyException";
+    this.message = `Invalid value for key ${key}`;
   }
 }
