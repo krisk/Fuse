5969ca716a52aa24fafa476bee02140518202593
Jeroen Claassens <support@favware.tech>
2023-01-27 21:01:07 +0100

    fix: build fixes

--- docs/.vuepress/components/Stories/stories.ts
+++ docs/.vuepress/components/Stories/stories.ts
@@ -1,4 +1,4 @@
-;[
+export default [
   {
     name: 'Notejoy',
     imgUrl: '/assets/img/products/notejoy@2x.png',
--- docs/.vuepress/components/Team/people.ts
+++ docs/.vuepress/components/Team/people.ts
@@ -1,4 +1,4 @@
-;[
+export default [
   {
     name: 'Kiro Risk',
     city: 'San Francisco, CA, USA',
